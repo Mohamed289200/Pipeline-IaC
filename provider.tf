f02a2934ce0dc7dc225481944e05a07ec8e0b86b
Mohamed289200 <mohamedabd092002@gmail.com>
2026-06-29 14:49:17 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,14 +8,7 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "terraform-state-mohamed-2026"
-    # key is injected at init time via -backend-config in the Jenkinsfile:
-    # terraform init -backend-config="key=${ENV}/terraform.tfstate"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-2"
-  }
-}
+
 
 provider "aws" {
   region = var.region
